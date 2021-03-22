0ea5be45f1dce71a8ab2d00a7bcf7de967df3921
Sergey Natarkin <snatarkin@gmail.com>
2021-03-22 13:14:20 +0300

    refactoring

--- Functions/src/main/java/com/epam/engx/cleancode/functions/task1/RegisterAccountAction.java
+++ Functions/src/main/java/com/epam/engx/cleancode/functions/task1/RegisterAccountAction.java
@@ -2,7 +2,7 @@ package com.epam.engx.cleancode.functions.task1;
 
 import com.epam.engx.cleancode.functions.task1.thirdpartyjar.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -15,7 +15,15 @@ public class RegisterAccountAction {
     private AccountManager accountManager;
 
     public void register(Account account) {
-        if (account.getName().length() <= 5){
+        verificationPasswordAndName(account);
+        account.setCreatedDate(new Date());
+        List<Address> addresses = Arrays.asList(account.getHomeAddress(), account.getWorkAddress(), account.getAdditionalAddress());
+        account.setAddresses(addresses);
+        accountManager.createNewAccount(account);
+    }
+
+    private void verificationPasswordAndName(Account account) {
+        if (account.getName().length() <= 5) {
             throw new WrongAccountNameException();
         }
         String password = account.getPassword();
@@ -25,14 +33,6 @@ public class RegisterAccountAction {
         if (passwordChecker.validate(password) != OK) {
             throw new WrongPasswordException();
         }
-
-        account.setCreatedDate(new Date());
-        List<Address> addresses = new ArrayList<Address>();
-        addresses.add(account.getHomeAddress());
-        addresses.add(account.getWorkAddress());
-        addresses.add(account.getAdditionalAddress());
-        account.setAddresses(addresses);
-        accountManager.createNewAccount(account);
     }
 
 
