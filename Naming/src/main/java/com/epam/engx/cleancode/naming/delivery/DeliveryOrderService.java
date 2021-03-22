20463be9c3c0300b7786ff5891fe1a5610ef8041
Sergey Natarkin <snatarkin@gmail.com>
2021-03-22 13:14:03 +0300

    renaming

--- Naming/src/main/java/com/epam/engx/cleancode/naming/delivery/DeliveryOrderService.java
+++ Naming/src/main/java/com/epam/engx/cleancode/naming/delivery/DeliveryOrderService.java
@@ -1,31 +1,31 @@
 package com.epam.engx.cleancode.naming.delivery;
 
 import com.epam.engx.cleancode.naming.IOrderService;
-import com.epam.engx.cleancode.naming.task1.thirdpartyjar.*;
+
 import com.epam.engx.cleancode.naming.thirdpartyjar.*;
 
 import java.util.List;
 
 public class DeliveryOrderService implements IOrderService {
 
-    private DeliveryService mDeliveryService;
+    private DeliveryService deliveryService;
 
-    private OrderFulfilmentService mOrderFulfilmentService;
+    private OrderFulfilmentService orderFulfilmentService;
 
-    public void submitOrder(Order pOrder) {
-        if (mDeliveryService.isDeliverable(pOrder)) {
-            List<Product> products = pOrder.getProducts();
-            mOrderFulfilmentService.fulfilProducts(products);
+    public void submitOrder(Order productsOrder) {
+        if (deliveryService.isDeliverable(productsOrder)) {
+            List<Product> products = productsOrder.getProducts();
+            orderFulfilmentService.fulfilProducts(products);
         } else {
             throw new NotDeliverableOrderException();
         }
     }
 
-    public void setDeliveryService(DeliveryService pDeliveryService) {
-        this.mDeliveryService = pDeliveryService;
+    public void setDeliveryService(DeliveryService deliveryServiceImpl) {
+        this.deliveryService = deliveryServiceImpl;
     }
 
-    public void setOrderFulfilmentService(OrderFulfilmentService pOrderFulfilmentService) {
-        this.mOrderFulfilmentService = pOrderFulfilmentService;
+    public void setOrderFulfilmentService(OrderFulfilmentService orderFulfilmentServiceImpl) {
+        this.orderFulfilmentService = orderFulfilmentServiceImpl;
     }
 }
